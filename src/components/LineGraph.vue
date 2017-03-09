32dc50cb64baa12d9eeb13c7da76a126495de7af
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2017-03-08 21:07:53 -0500

    first commit

---
+++ src/components/LineGraph.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Test Graph
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
