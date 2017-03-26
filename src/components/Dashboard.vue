5d911bd7e781fb8a9c8516c8d96510269fffc917
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2017-03-26 11:48:41 -0400

    get reactive rendering working

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,23 +2,42 @@
   <div>
     <h1>Dashboard</h1>
     <p>Chart.js + Vue.js dashboard</p>
-    <pie-chart></pie-chart>
+    <bar-graph :chartData="dataCollection" :width="250" :height="250"></bar-graph>
+    <input type="text" v-model="testVal" v-on:keyup.enter="fillData()">
   </div>
 </template>
 
 <script>
-  import LineGraph from './LineGraph'
-  import PieChart from './PieChart'
+  import barGraph from './barGraph'
   export default {
+    components: {
+      'bar-graph': barGraph
+    },
     data () {
-      return {}
+      return {
+        dataCollection: null,
+        testVal: 100
+      }
+    },
+    mounted () {
+      this.fillData()
     },
     methods: {
-
-    },
-    components: {
-      'line-graph': LineGraph,
-      'pie-chart': PieChart
+      fillData () {
+        this.dataCollection = {
+          labels: ['data1'],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [100]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1))
+      }
     }
   }
 </script>
---
+++ src/components/barGraph.vue
@@ -0,0 +1,12 @@
+<script>
+  import { Bar, mixins } from 'vue-chartjs'
+  const { reactiveProp } = mixins
+
+  export default Bar.extend({
+    mixins: [reactiveProp],
+    mounted () {
+      // this.chartData is created in the mixin
+      this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false})
+    }
+  })
+</script>
